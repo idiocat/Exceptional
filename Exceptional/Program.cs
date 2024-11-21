ae7921b8bfa9f9cf10a92ca5dc0cd68818105b9e
idiocy <idiocy@DESKTOP-PP32I9J>
2024-11-21 12:17:52 +0300

    Add project files.

---
+++ Exceptional/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace exceptional;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Exception[] exces = {
+            new DataMisalignedException("DataMisalignedException"),
+            new BarrierPostPhaseException("BarrierPostPhaseException"),
+            new LockRecursionException("LockRecursionException"),
+            new TimeZoneNotFoundException("LockRecursionException"),
+            new ExceptionalException("ExceptionalException") };
+        foreach (Exception ex in exces)
+        {
+            try { throw ex; }
+            catch (Exception ee) { Console.WriteLine(ee.Message); }
+            finally { Console.Write(string.Empty + Environment.NewLine); }
+        }
+        Console.ReadKey();
+    }
+
+    public class ExceptionalException : Exception
+    {
+        public ExceptionalException() { }
+        public ExceptionalException(string message) : base(message + " (this one is very much like others, but it's mine)") { }
+        public ExceptionalException(string message,  Exception innerException)
+            : base(message + " (this one is very much like others, but it's mine)", innerException) { }
+    }
+}
